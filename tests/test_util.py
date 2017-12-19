c2b480b3fb0c651b897a03670457fb84647219c2
Jonathan MacCarthy <jkmacc@lanl.gov>
2017-12-19 10:47:13 -0700

    Add stransform test and data

---
+++ tests/test_util.py
@@ -0,0 +1,16 @@
+"""
+Test core stockwell functionality.abs
+
+"""
+import os
+
+import numpy as np
+from stockwell import stransform, istransform
+
+here = os.path.dirname(os.path.abspath(__file__))
+data = np.loadtxt(os.path.sep.join([here, 'data', 'BW.RJOB..EHZ.txt']))
+
+def test_stransform():
+    S = stransform(data, Fs=100)
+    s = istransform(S, Fs=100)
+    assert np.allclose(s, data)
