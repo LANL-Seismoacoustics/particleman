0bb383b717dbe2b5a78f09238332623be937cb15
Jonathan MacCarthy <jkmacc@lanl.gov>
2020-11-24 10:47:59 -0700

    Update Sphinx config for recommonmark

--- docs/src/conf.py
+++ docs/src/conf.py
@@ -43,6 +43,7 @@ extensions = [
     'sphinx.ext.viewcode',
     'sphinx.ext.githubpages',
     'sphinx.ext.napoleon',
+    'recommonmark',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
